6d45826899078d222a61d0e02927720cdbea8afc
Faisal <zkryfysl@gmail.com>
2024-10-16 22:07:39 +0200

    convert cases code into func

--- script.js
+++ script.js
@@ -4,8 +4,7 @@ function saveTasks() {
 }
 
 const cls = () => localStorage.clear();
-
-const tasksManager = _ =>{
+const tasksList = ()=>{
     console.log("")
     console.log("Task Manager Menu :");
     console.log("1. Add Tasks")
@@ -17,90 +16,109 @@ const tasksManager = _ =>{
     console.log("7.To clear local storge")
     console.log("8. Exit")
     console.log("")
+}
+const addTask = ()=>{
+    let add = prompt("Enter Your Task Description ");
+    console.log("Task Added : " + add)
+    add = add + " : " + "[Not Completed]"
+    Tasks.push(add)
+}
+const view = ()=>{
+    if(Tasks.length == 0){
+        console.log("No Tasks Found");
+    }else{
+        console.log("Your Tasks :");
+        Tasks.map((task , index)=>{
+            index++;
+            console.log(index +"- " + task)
+        })
+    }
+}
+const toggle =()=>{
+    let id = prompt("Enter the task ID to toggle completion:");
+    Tasks.map((task, index) => {
+        if (id == index + 1) {
+            if(task.includes("Not Completed")){
+
+                console.log("Task " + task.slice(0, task.indexOf(":")) + " is now marked as completed");
+                task = task.replace("[Not Completed]", "[Completed]");
+                Tasks.splice(index, 1, task);
+            }else if(task.includes("Completed")){
+                console.log("Task " + task.slice(0, task.indexOf(":")) + " is now marked as Not completed");
+                task = task.replace("[Completed]", "[Not Completed]");
+                Tasks.splice(index, 1, task);
+
+            }
+        }
+    });
+}
+const edit = ()=>{
+    let id1 = prompt("Enter the task ID to Edit:");
+    let taskToEdit = Tasks.find((task, index) => index + 1 == id1);
+
+    if (taskToEdit) {
+        let newTask = prompt("Edit Your Task Description:");
+        let taskIndex = Tasks.indexOf(taskToEdit);
+        Tasks.splice(taskIndex, 1, newTask + " : " + "[Not Completed]");
+        console.log("Task updated successfully.");
+    } else {
+        console.log("Task Not Found, please Enter Correct Task ID");
+    }
+}
+const deleted = ()=>{
+    let id2 = prompt("Enter the task ID to Delete:");
+    let taskToDelete = Tasks.find((task, index) => index + 1 == id2);
+
+    if (taskToDelete) {
+        let taskIndex = Tasks.indexOf(taskToDelete);
+        Tasks.splice(taskIndex, 1);
+        console.log("Task deleted successfully.");
+    } else {
+        console.log("Task Not Found, please Enter Correct Task ID");
+    }
+}
+const search = ()=>{
+    let search = prompt("Enter the task name to search:");
+    let found = false;
+    Tasks.filter((task) => {
+        if (task.slice(0, task.indexOf(":")).includes(search)) {
+            console.log("Task Found: " + task);
+            found = true;
+        }
+    });
+    if (!found) {
+        console.log("Task Not Found");
+    }
+}
+const tasksManager = _ =>{
+    tasksList();
     let taskNum = prompt("Enter Your Choice(1-8)",'');
     switch (taskNum) {
         case "1":
-            let add = prompt("Enter Your Task Description ");
-            console.log("Task Added : " + add)
-            add = add + " : " + "[Not Completed]"
-            Tasks.push(add)
+            addTask();
             saveTasks();
             tasksManager();
             break;
         case "2":
-            if(Tasks.length == 0){
-                console.log("No Tasks Found");
-            }else{
-                console.log("Your Tasks :");
-                Tasks.map((task , index)=>{
-                    index++;
-                    console.log(index +"- " + task)
-                })
-            }
+            view();
             tasksManager();
             break;
         case "3":
-            let id = prompt("Enter the task ID to toggle completion:");
-            Tasks.map((task, index) => {
-                if (id == index + 1) {
-                    if(task.includes("Not Completed")){
-
-                        console.log("Task " + task.slice(0, task.indexOf(":")) + " is now marked as completed");
-                        task = task.replace("[Not Completed]", "[Completed]");
-                        Tasks.splice(index, 1, task);
-                    }else if(task.includes("Completed")){
-                        console.log("Task " + task.slice(0, task.indexOf(":")) + " is now marked as Not completed");
-                        task = task.replace("[Completed]", "[Not Completed]");
-                        Tasks.splice(index, 1, task);
-
-                    }
-                }
-            });
+            toggle();
             saveTasks();
             tasksManager();
             break;
         case "4":
-            let id1 = prompt("Enter the task ID to Edit:");
-            let taskToEdit = Tasks.find((task, index) => index + 1 == id1);
-        
-            if (taskToEdit) {
-                let newTask = prompt("Edit Your Task Description:");
-                let taskIndex = Tasks.indexOf(taskToEdit);
-                Tasks.splice(taskIndex, 1, newTask + " : " + "[Not Completed]");
-                console.log("Task updated successfully.");
-            } else {
-                console.log("Task Not Found, please Enter Correct Task ID");
-            }
-        
+            edit();
             saveTasks();
             tasksManager();
             break;
         case "5":
-            let id2 = prompt("Enter the task ID to Delete:");
-            let taskToDelete = Tasks.find((task, index) => index + 1 == id2);
-
-            if (taskToDelete) {
-                let taskIndex = Tasks.indexOf(taskToDelete);
-                Tasks.splice(taskIndex, 1);
-                console.log("Task deleted successfully.");
-            } else {
-                console.log("Task Not Found, please Enter Correct Task ID");
-            }
-            
+            deleted();
             tasksManager();
             break;
         case "6":
-            let search = prompt("Enter the task name to search:");
-            let found = false;
-            Tasks.filter((task) => {
-                if (task.slice(0, task.indexOf(":")).includes(search)) {
-                    console.log("Task Found: " + task);
-                    found = true;
-                }
-            });
-            if (!found) {
-                console.log("Task Not Found");
-            }
+            search();
             tasksManager();
             break;
         case "7":
